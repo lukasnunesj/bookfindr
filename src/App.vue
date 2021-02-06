2a12cdd1d6e6da195ad4b2b3e7cf2ba34165f105
Lucas Nunes <lucas.nunes@kabum.com.br>
2021-02-06 18:59:49 -0300

    feat: Adding logic for search

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-<div>
-    <header>
-        <Navbar/>
-    </header>
-    <main>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    
+    <div>
+        <header>
+            <Navbar />
+        </header>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <Searchbox @searchFor="searchBook" />
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-7">
+                        <UnorderedList>
+                            <BookItem
+                                v-for="book in bookList"
+                                :key="book.id"
+                                :author="book.volumeInfo.authors | authorsArrayToString"
+                                :title="book.volumeInfo.title"
+                                :imgUrl="book.volumeInfo.imageLinks"
+                                :link="book.volumeInfo.infoLink"
+                                :publishedDate="book.volumeInfo.publishedDate | getDate"
+                            />
+                        </UnorderedList>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-</div>
-
+        </main>
+    </div>
 </template>
 
 <script>
-    import Navbar from "./components/header/Navbar"
+    import Navbar from "./components/header/Navbar";
+    import Searchbox from "./components/searchbox/Searchbox";
+    import UnorderedList from "./ui/List/UnorderedList";
+    import BookItem from "./ui/List/BookItem";
+
+    import axios from "axios";
 
     export default {
         name: "App",
         components: {
-            Navbar
+            Navbar,
+            Searchbox,
+            UnorderedList,
+            BookItem,
+        },
+        data: function() {
+            return {
+                bookList: [],
+            };
+        },
+        methods: {
+            searchBook(term) {
+                axios.get(`https://www.googleapis.com/books/v1/volumes?q=${term}`).then((result) => {
+                    this.bookList = result.data.items;
+                });
+            },
+        },
+        filters: {
+            authorsArrayToString: function(authorsArray) {
+                console.log(authorsArray);
+                if (!authorsArray) {
+                    return "by unknown";
+                }
+                authorsArray.forEach((element, index) => {
+                    authorsArray[index] = element.toLowerCase().replace(/\b\w/g, (l) => l.toUpperCase());
+                });
+                return "by " + authorsArray.join(", ");
+            },
+            getDate: function(dateString) {
+                if (!dateString) {
+                    return "unknown publishing year";
+                }
+                const [year] = dateString.split("-");
+                return "published at " + year;
+            },
         },
     };
 </script>
