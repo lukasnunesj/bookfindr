ffadc58e523b95cb4911db232f7ba33ca2f3a4c8
Lucas Nunes <lucas.nunes@kabum.com.br>
2021-02-06 18:59:29 -0300

    refact: Changing order for elements

--- src/ui/Navbar/NavbarBrand.vue
+++ src/ui/Navbar/NavbarBrand.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <a class="navbar-brand" :href="link">{{text}}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" :data-bs-target="'#'+target" :aria-controls="target" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <a class="navbar-brand" :href="link">{{text}}</a>
     </div>
 </template>
 
