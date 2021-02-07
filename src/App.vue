74c01414a836b822bea09e71eed5019b318d2002
Lucas Nunes <lucas.nunes@kabum.com.br>
2021-02-06 22:30:32 -0300

    refact: Adding routing and views

--- src/App.vue
+++ src/App.vue
@@ -3,107 +3,25 @@
         <header>
             <Navbar />
         </header>
-        <main>
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <Searchbox @searchFor="searchBook" />
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-7">
-                        <UnorderedList>
-                            <BookItem
-                                v-for="book in bookList"
-                                :key="book.id"
-                                :author="book.volumeInfo.authors | authorsArrayToString"
-                                :title="book.volumeInfo.title"
-                                :imgUrl="book.volumeInfo.imageLinks"
-                                :link="book.volumeInfo.infoLink"
-                                :publishedDate="book.volumeInfo.publishedDate | getDate"
-                            />
-                        </UnorderedList>
-                    </div>
-                </div>
-            </div>
-        </main>
-        <footer class="footer mt-auto py-3">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <p class="text-center pt-3">© Copyright {{ new Date().getFullYear() }} Lucas Nunes. All rights reserved.</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col text-center">
-                        <a href="#" class="footer-link" target="_blank"> 
-                            <github-icon size="1.5x" class="mx-2"></github-icon> 
-                        </a>
-                        <a href="#" class="footer-link" target="_blank"> 
-                            <linkedin-icon size="1.5x"></linkedin-icon>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <router-view/>
+        <Footer/>
     </div>
 </template>
 
 <script>
     import Navbar from "./components/header/Navbar";
-    import Searchbox from "./components/searchbox/Searchbox";
-    import UnorderedList from "./ui/List/UnorderedList";
-    import BookItem from "./ui/List/BookItem";
-    import { GithubIcon, LinkedinIcon } from 'vue-feather-icons'
-    import axios from "axios";
+    import Footer from "./components/footer/Footer";
 
     export default {
         name: "App",
         components: {
             Navbar,
-            Searchbox,
-            UnorderedList,
-            BookItem,
-            GithubIcon,
-            LinkedinIcon,
+            Footer
         },
-        data: function() {
-            return {
-                bookList: [],
-            };
-        },
-        methods: {
-            searchBook(term) {
-                axios.get(`https://www.googleapis.com/books/v1/volumes?q=${term}`).then((result) => {
-                    this.bookList = result.data.items;
-                });
-            },
-        },
-        filters: {
-            authorsArrayToString: function(authorsArray) {
-                console.log(authorsArray);
-                if (!authorsArray) {
-                    return "by unknown";
-                }
-                authorsArray.forEach((element, index) => {
-                    authorsArray[index] = element.toLowerCase().replace(/\b\w/g, (l) => l.toUpperCase());
-                });
-                return "by " + authorsArray.join(", ");
-            },
-            getDate: function(dateString) {
-                if (!dateString) {
-                    return "unknown publishing year";
-                }
-                const [year] = dateString.split("-");
-                return "published at " + year;
-            },
-        },
+        
     };
 </script>
 
 <style scoped>
-.footer-link {
-    text-decoration: none;
-    color: #212529;
-}
+
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8 mt-5">
+            <h3 class="text-center">About</h3>
+            <br>
+            <br>
+            <p>
+                This site is very simple, you type some terms and click to search. It will call Google Books' API using Axios to retrieve data wich matches with the terms searched.
+            </p>
+            <p>
+                I make this project using <b>VueJs</b> because I want to pratice my knownlodge in this framework that I like ver much!
+            </p>
+            <p>
+                I chose to use Bootstrap because I didn't want to worry about CSS in this especific project. I wanted to keep it simple so I foucused most in Vue aspects.
+            </p>
+            <p>
+                <i>Vue Router</i> was used to implement navigation between pages.
+            </p>
+            <p>
+                The Idea for this was taken from the 
+                <a class="text-decoration-none" href="https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Book-Finder-App.md" target="_blank"> 
+                    <b>APP IDEAS</b>
+                </a> repository.
+            </p>
+        </div>
+    </div>
+  </div>
+</template>
