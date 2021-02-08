6488175ef99c56c9d7c90d67469ddee9b7620fa0
Lucas Nunes <lucas.nunes@kabum.com.br>
2021-02-07 22:03:50 -0300

    fix: Correcting mispellings

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,13 +6,13 @@
             <br>
             <br>
             <p>
-                This site is very simple, you type some terms and click "search". It will call Google Books' API using Axios to retrieve data which match with the terms searched.
+                This site is very simple, you type some terms and click "search". It will call Google Books' API using Axios to retrieve data matching with the terms searched.
             </p>
             <p>
-                I made this project using <b>VueJs</b> because I want to practice my knowledge in this framework that I like very much!
+                I made this project using <b>VueJs</b> because I wanted to practice my knowledge in this framework that I like so much!
             </p>
             <p>
-                I chose to use Bootstrap because I didn't want to worry about CSS in this specific project. Furthermore, I wanted to keep it simple, so I focused mostly on Vue aspects.
+                I chose to use Bootstrap because I didn't want to worry about CSS in this specific project. Furthermore, I wanted to keep it simple, so I focused mostly on the Vue aspects.
             </p>
             <p>
                 <i>Vue Router</i> was used to implement navigation between pages.
